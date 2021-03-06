e4775a09368f32e26705f91b4d2c6d9fee08eea8
Snowy <snowy@snowymouse.com>
2021-03-06 01:16:35 -0600

    inet_ntop checks the address part only

--- src/socket.cpp
+++ src/socket.cpp
@@ -6,7 +6,16 @@ namespace Mousygem {
     std::string SocketAddress::ip_address() const {
         // Do it!
         char ip_address[1024];
-        auto *pt = inet_ntop(this->ss.ss_family, &this->ss, ip_address, sizeof(ip_address));
+        const char *pt = nullptr;
+        switch(this->ss.ss_family) {
+            case AF_INET:
+                pt = inet_ntop(AF_INET, &reinterpret_cast<const sockaddr_in *>(&this->ss)->sin_addr, ip_address, sizeof(ip_address));
+                break;
+            case AF_INET6:
+                pt = inet_ntop(AF_INET6, &reinterpret_cast<const sockaddr_in6 *>(&this->ss)->sin6_addr, ip_address, sizeof(ip_address));
+                break;
+        }
+        
         if(pt == nullptr) {
             throw std::runtime_error("inet_ntop() failed");
         }
